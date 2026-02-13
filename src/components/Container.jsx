2584beae73a106f190eccb05115cc9a7f2c3d916
saikat <saikat@macs-MacBook-Pro.local>
2026-02-13 14:06:09 +0600

    changes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children, className }) => {
   return (
-    <div className={`w-full max-w-[1800px] mx-auto ${className}`}>
+    <div className={`w-full max-w-[1000px] mx-auto m-2 rounded-2xl ${className}`}>
       {children}
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <Container className="flex justify-evenly items-center">
+      <Container className="flex justify-center gap-4 items-center bg-white h-16 m-4 border-2 border-black">
         <Link to="/">
-          <button className="cursor-pointer">Home</button>
+          <button className="cursor-pointer border-2 border-black p-2 rounded-2xl hover:bg-black hover:text-white hover:border-2 hover:border-white duration-300 ease-out ">Home</button>
         </Link>
         <Link to="/about">
-          <button className="cursor-pointer">About</button>
+          <button className="cursor-pointer border-2 border-black p-2 rounded-2xl  hover:bg-black hover:text-white hover:border-2 hover:border-white duration-300 ease-out ">About</button>
         </Link>
         <Link to="/contact">
-          <button className="cursor-pointer">Contact</button>
+          <button className="cursor-pointer border-2 border-black p-2 rounded-2xl  hover:bg-black hover:text-white hover:border-2 hover:border-white duration-300 ease-out ">Contact</button>
         </Link>
       </Container>
     </nav>
